4cb8a18ebf3a7c7e41dcccb67fb6d1b7b6964b6e
Eric Moynihan <git@moynihan.io>
2020-05-20 22:20:18 +0100

    Added gitdepth

--- stonks.rb
+++ stonks.rb
@@ -1,5 +1,16 @@
 require 'formula'
 
+class GitNoDepthDownloadStrategy < GitDownloadStrategy
+    # We need the .git folder for it's information, so we clone the whole thing
+    def stage
+        dst = Dir.getwd
+        @clone.cd do
+            reset
+            safe_system 'git', 'clone', '.', dst
+        end
+    end
+end
+  
 class Stonks < Formula
     homepage 'https://github.com/ericm/stonks'
 
