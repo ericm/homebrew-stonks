11b213240946b09d4eaf62c9f2c0feaf29e6eb64
Eric Moynihan <git@moynihan.io>
2020-05-22 15:05:53 +0100

    Fixin build

--- Formula/stonks.rb
+++ Formula/stonks.rb
@@ -1,22 +1,13 @@
 require 'formula'
 
-class GitNoDepthDownloadStrategy < GitDownloadStrategy
-    # We need the .git folder for it's information, so we clone the whole thing
-    def stage
-        dst = Dir.getwd
-        @clone.cd do
-            reset
-            safe_system 'git', 'clone', '.', dst
-        end
-    end
-end
   
 class Stonks < Formula
     homepage 'https://github.com/ericm/stonks'
 
+    url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.3"
+
     stable do
-        url 'https://github.com/ericm/stonks.git',
-          :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "v1.0.3"
+        url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.3"
         version "1.0.3"
     end
 
