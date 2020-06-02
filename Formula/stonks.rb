bfab99317f9f0495207e89412488811aa7ca9a89
Eric Moynihan <git@moynihan.io>
2020-06-02 09:47:43 +0100

    Release 1.0.8

--- Formula/stonks.rb
+++ Formula/stonks.rb
@@ -4,11 +4,11 @@ require 'formula'
 class Stonks < Formula
     homepage 'https://github.com/ericm/stonks'
 
-    url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.7"
+    url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.8"
 
     stable do
-        url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.7"
-        version "1.0.7"
+        url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.8"
+        version "1.0.8"
     end
 
     depends_on "golang"
