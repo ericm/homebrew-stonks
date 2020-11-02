b64847914230222e0062b11ebbb05401f0f8f797
Eric Moynihan <git@moynihan.io>
2020-11-02 15:43:46 +0000

    Release 1.0.9

--- Formula/stonks.rb
+++ Formula/stonks.rb
@@ -4,10 +4,10 @@ require 'formula'
 class Stonks < Formula
     homepage 'https://github.com/ericm/stonks'
 
-    url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.8"
+    url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.9"
 
     stable do
-        url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.8"
+        url 'https://github.com/ericm/stonks.git', :using => :git, :tag => "v1.0.9"
         version "1.0.8"
     end
 
